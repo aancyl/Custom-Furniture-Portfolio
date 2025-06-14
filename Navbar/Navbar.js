1f757aca9205ffb8bdc03d1966f7504167b9c8cf
aancyl <aancyllioneldsouza@gmail.com>
2025-06-14 18:21:41 +0400

    Deleting extra files and fixing Status bar issues

--- Navbar/Navbar.js
+++
@@ -1,8 +0,0 @@
-function toggleMenu() {
-    var overlay = document.getElementById("overlay");
-    if (overlay.style.height === "100%") {
-        overlay.style.height = "0%";
-    } else {
-        overlay.style.height = "100%";
-    }
-}
